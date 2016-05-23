99aa80b6a1b85567e8b9f4247ba49980d0f4ed3c
Jan Leonhardt <janleo@gmail.com>
2016-05-23 12:02:27 +0100

    5

--- scritps.js
+++ scritps.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-    $('#myTable').DataTable();
-});
+function parseTable(file,callback) {
+    Papa.parse(file, {
+        download: true,
+        header: true,
+        complete: function(results) {
+            callback(results.data)
+        }
+    })
+};
+
+function setTable(){
+    parseTable('/season/batting.csv',function(n){
+        //$('#table').DataTable({
+        //})
+        console.log(n)
+    });
+}
+
+setTable();
